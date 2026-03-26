947bb02567228989e149b9d090cbd5e7e75e523c
Bazaars <102147094+BazaarsBZR@users.noreply.github.com>
2026-03-26 21:32:57 +0200

    Update BZR.sol

--- contracts/BZR.sol
+++ contracts/BZR.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity ^0.8.26;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/extensions/ERC20Burnable.sol";
